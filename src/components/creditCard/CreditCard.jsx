834531a465c6073c67c2d49fc47fc7eb59cd28b7
Ozod <o.bukhorizoda@mail.ru>
2023-12-21 09:22:31 +0500

    styled credit card

--- src/components/creditCard/CreditCard.jsx
+++ src/components/creditCard/CreditCard.jsx
@@ -3,9 +3,9 @@ import styles from "./CreditCard.module.css";
 const CreditCard = () => {
   return (
     <section className={styles.credit_card_container}>
-      <article>
-        <article>Front-card</article>
-        <article>Back-card</article>
+      <article className={styles.cards_holder}>
+        <article className={styles.front_card}>Front-card</article>
+        <article className={styles.back_card}>Back-card</article>
       </article>
     </section>
   );
