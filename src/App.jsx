8f2f70107d5520c6d98da81b8aa911da74705905
Ozod <o.bukhorizoda@mail.ru>
2023-12-21 14:10:16 +0500

    fixed the issue with onchange fns

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import CreditCard from "./components/creditCard/CreditCard";
-import Form from "./components/Form/Form";
-import { createContext } from "react";
-export const ChangeStates = createContext(null);
+import { CreditCard } from "./components/creditCard/CreditCard";
 
 function App() {
-  const [cardHolder, setCardHolder] = useState("Card holder");
   return (
     <>
-      <ChangeStates.Provider value={[cardHolder, setCardHolder]}>
-        <CreditCard>
-          <Form />
-        </CreditCard>
-      </ChangeStates.Provider>
+      <CreditCard />
     </>
   );
 }
--- src/components/creditCard/CreditCard.jsx
+++ src/components/creditCard/CreditCard.jsx
@@ -1,25 +1,89 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import styles from "./CreditCard.module.css";
-import { ChangeStates } from "../../App";
-import { useContext } from "react";
+export const CreditCard = () => {
+  const [cardHolder, setCardHolder] = useState("Card Holder");
+  const [cardNumber, setCardNumber] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
 
-const CreditCard = ({ children }) => {
-  const [cardHolder] = useContext(ChangeStates);
+  const handleCardNumber = (e) => {
+    const value = e.target.value.replace(/\D/g, "");
+    const formattedValue = value.replace(/(\d{4})/g, "$1 ").trim();
+    setCardNumber(formattedValue);
+  };
   return (
     <section className={styles.credit_card_container}>
       <article className={styles.cards_holder}>
         <article className={styles.front_card}>
-          <h1 className={styles.number}>1234 4567 7899 1111</h1>
+          <h1 className={styles.number}>{cardNumber}</h1>
           <h3 className={styles.name}>{cardHolder}</h3>
-          <h4 className={styles.expire_date}>05/26</h4>
+          <h4 className={styles.expire_date}>
+            {month}/{year}
+          </h4>
         </article>
         <article className={styles.back_card}>
-          <h3 className={styles.cvc}>649</h3>
+          <h3 className={styles.cvc}>{cvc}</h3>
         </article>
       </article>
-      {children}
+      {/* form container */}
+      <div className={styles.form_container}>
+        <form className={styles.form}>
+          <label>Card number</label>
+          <input
+            type="text"
+            placeholder="Enter card number"
+            maxLength={19}
+            value={cardNumber}
+            onChange={(e) => handleCardNumber(e)}
+          />
+          <label>Card holder name</label>
+          <input
+            type="text"
+            placeholder="Enter card holder name"
+            maxLength={19}
+            value={cardHolder}
+            onChange={(e) => setCardHolder(e.target.value)}
+          />
+          <h3 className={styles.expire}>Expire Date</h3>
+          <div className={styles.m_y_cvc}>
+            <div className={styles.expire_label}>
+              <label>MM</label>
+              <input
+                type="text"
+                placeholder="MM"
+                className={styles.small_input}
+                maxLength={2}
+                value={month}
+                onChange={(e) => setMonth(e.target.value)}
+              />
+            </div>
+            <div className={styles.expire_label}>
+              <label>YY</label>
+              <input
+                type="text"
+                placeholder="YY"
+                className={styles.small_input}
+                maxLength={4}
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+              />
+            </div>
+            <div className={styles.expire_label}>
+              <label>CVC</label>
+              <input
+                type="text"
+                placeholder="CVC"
+                className={styles.big_input}
+                maxLength={3}
+                value={cvc}
+                onChange={(e) => setCvc(e.target.value)}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
-
-export default CreditCard;
