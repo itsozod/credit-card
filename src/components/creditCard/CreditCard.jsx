adaafae0b6e21db3a63fdd89f2d47a38c52efbb9
Ozod <o.bukhorizoda@mail.ru>
2023-12-21 10:48:21 +0500

    styled front-card data

--- src/components/creditCard/CreditCard.jsx
+++ src/components/creditCard/CreditCard.jsx
@@ -6,7 +6,9 @@ const CreditCard = ({ children }) => {
     <section className={styles.credit_card_container}>
       <article className={styles.cards_holder}>
         <article className={styles.front_card}>
+          <h1 className={styles.number}>1234 4567 7899 1111</h1>
           <h3 className={styles.name}>Ozod Bukhorizoda</h3>
+          <h3 className={styles.expire_date}>05/26</h3>
         </article>
         <article className={styles.back_card}>Back-card</article>
       </article>
